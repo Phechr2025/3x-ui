4af4aac607ca0035a7461d6a575221017fd451ae
Phechr2025 <duang.dees12@gmail.com>
2026-03-08 10:45:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -154,7 +154,7 @@ install_x-ui() {
     cd /usr/local/
 
     if [ $# == 0 ]; then
-        tag_version=$(curl -Ls --retry 5 --retry-delay 3 https://api.github.com/repos/io-vpn/3x-ui/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        tag_version=$(curl -Ls --retry 5 --retry-delay 3 https://api.github.com/repos/Phechr2025/3x-ui/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$tag_version" ]]; then
             echo -e "${red}Failed to fetch x-ui version, it may be due to GitHub API restrictions, please try it later${plain}"
             exit 1
